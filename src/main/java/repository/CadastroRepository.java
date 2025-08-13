d117b0949ed576e0514a78a5ea0fe7f9d090a177
Carolyne Rossetti <artdraw.edit@gmail.com>
2025-08-13 15:38:20 -0300

    Criando Repository de cada entidade

---
+++ src/main/java/repository/CadastroRepository.java
@@ -0,0 +1,26 @@
+package repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import model.entities.Cadastro;
+import model.enums.TipoCadastro;
+
+@Repository
+public interface CadastroRepository extends JpaRepository<CadastroRepository,Long> {
+	
+	//Buscar cadastro pelo login
+    Optional<Cadastro> findByLogin(String login);
+    
+    //Verificar se existe cadastro com o login informado
+    boolean existsByLogin(String login);
+
+    //Buscar cadastro pelo tipo de cadastro
+    List<Cadastro> findByTipoCadastro(TipoCadastro tipoCadastro);
+
+    //Buscas cadastro pelo login e tipo de cadastro
+    Optional<Cadastro> findByLoginAndTipoCadastro(String login, TipoCadastro tipoCadastro);
+}
---
+++ src/main/java/repository/ProfissionalSaudeRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfissionalSaudeRepository extends JpaRepository<ProfissionalSaudeRepository, Long>{
+
+}
